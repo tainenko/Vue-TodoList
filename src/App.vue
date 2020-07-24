89f7cc62c3a413cda0aaa09ffc9352f03ccd6fce
ko tainen <TonyK@hyvesolutions.com>
2020-07-24 15:55:36 +0800

    update todos with vue-for

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,20 @@
         },
         data() {
             return {
-                todos:[
+                todos: [
                     {
                         id: 1,
                         title: "Todo One",
+                        completed: false
+                    },
+                    {
+                        id: 2,
+                        title: "Todo Two",
+                        completed: false
+                    },
+                    {
+                        id: 3,
+                        title: "Todo Three",
                         completed: false
                     }
                 ]
