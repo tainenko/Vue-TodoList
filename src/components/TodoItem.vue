ddf07e80f60fd08814b059d7aefc8024d6731706
ko tainen <TonyK@hyvesolutions.com>
2020-07-24 16:48:38 +0800

    Add TodoItem.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "TodoItem"
+    }
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-completed {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+
+</style>
